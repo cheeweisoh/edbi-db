f0b4c291b046da5e79f767b251ef4fa26e8e940a
cheeweisoh <sohcheewei@u.nus.edu>
2026-03-05 03:55:11 +0000

    bug fixes

--- notebooks/ingest_to_landing.py
+++ notebooks/ingest_to_landing.py
@@ -9,7 +9,7 @@ file_format = dbutils.widgets.get("file_format")
 VOLUME_ROOT = "/Volumes/edbi_teamg01"
 SOURCE_PATH = f"{VOLUME_ROOT}/landing/{subfolder}/"
 
-from pyspark.sql.functions import input_file_name, current_timestamp, regexp_extract
+from pyspark.sql.functions import col, current_timestamp, regexp_extract
 
 df = (
     spark.readStream.format("cloudFiles")
@@ -19,7 +19,7 @@ df = (
     .load(SOURCE_PATH)
 )
 
-df = df.withColumn("_source_file", input_file_name()).withColumn("_ingested_at", current_timestamp()).withColumn("_file_date", regexp_extract(input_file_name(), r"(\d{8})\.csv", 1))
+df = df.withColumn("_source_file", col("_metadata.file_path")).withColumn("_ingested_at", current_timestamp()).withColumn("_file_date", regexp_extract(col("_metadata.file_path"), r"(\d{8})\.csv", 1))
 
 (
     df.writeStream.format("delta")
@@ -27,5 +27,5 @@ df = df.withColumn("_source_file", input_file_name()).withColumn("_ingested_at",
     .option("checkpointLocation", f"{SOURCE_PATH}_checkpoint")
     .option("mergeSchema", "true")
     .trigger(availableNow=True)
-    .toTable(f"my_catalog.landing.{source_name}")
+    .toTable(f"edbi_teamg01.landing.{source_name}")
 )
