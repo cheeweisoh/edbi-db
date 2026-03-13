84dfd4812387b01f3e08bc34c188041983479ca8
cheewei <sohcheewei@u.nus.edu>
2026-03-13 19:53:44 +0800

    additional checks for info extracted

--- models/silver/qa/qa_info_extracted.sql
+++ models/silver/qa/qa_info_extracted.sql
@@ -25,9 +25,14 @@ WITH base AS (
         _bronze_loaded_at,
 
         -- quality flags
+        CASE WHEN case_no IS NULL THEN false ELSE true END AS _dq_missing_case_no,
+        CASE WHEN charge_no IS NULL THEN false ELSE true END AS _dq_missing_charge_no,
+        CASE WHEN entity_name IS NULL THEN false ELSE true END AS _dq_missing_entity_name,
+        CASE WHEN entity_type IS NULL THEN false ELSE true END AS _dq_missing_entity_type,
         CASE WHEN entity_type NOT IN ('Accused Person', 'Victim') THEN false ELSE true END AS _dq_invalid_entity_type,
         CASE WHEN entity_age NOT BETWEEN 0 AND 120 THEN false ELSE true END AS _dq_invalid_entity_age,
-        CASE WHEN entity_gender NOT IN ('M', 'F') THEN false ELSE true END AS _dq_invalid_entity_gender
+        CASE WHEN entity_gender NOT IN ('M', 'F') THEN false ELSE true END AS _dq_invalid_entity_gender,
+        CASE WHEN offence_group IS NULL THEN false ELSE true AS _dq_missing_offence_group
     FROM {{ ref('info_extracted') }}
     WHERE _rejected_reason IS NULL
     {% if is_incremental() %}
@@ -38,9 +43,14 @@ WITH base AS (
 SELECT
     *,
     CASE
+        WHEN _dq_missing_case_no = true THEN true
+        WHEN _dq_missing_charge_no = true THEN true
+        WHEN _dq_missing_entity_name = true THEN true
+        WHEN _dq_missing_entity_type = true THEN true
         WHEN _dq_invalid_entity_type = true THEN true
         WHEN _dq_invalid_entity_age = true THEN true
         WHEN _dq_invalid_entity_gender = true THEN true
+        WHEN _dq_missing_offence_group = true THEN true
         ELSE false
     END AS is_valid_row
 FROM base       
