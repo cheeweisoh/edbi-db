d210407d419f964d3021526f6d1afe75b94ffc67
cheewei <sohcheewei@u.nus.edu>
2026-03-20 13:19:10 +0800

    bug fix

--- models/silver/dim_case.sql
+++ models/silver/dim_case.sql
@@ -13,14 +13,14 @@ WITH cases AS (
         TRY_CAST(accused_dob AS DATE) AS accused_dob,
         case_no,
         case_title,
-        directorate,
+        directorate
     FROM {{ ref('qa_ext_criliti_sc') }}
     WHERE is_valid_row = TRUE
 ),
 
 cases_with_accused AS (
-    SELECT
-        cases.*
+    SELECT DISTINCT
+        cases.*,
         persons.person_skey
     FROM cases
     LEFT JOIN {{ ref('dim_person') }} AS persons
@@ -34,5 +34,5 @@ SELECT
     person_skey AS accused_skey,
     case_no,
     case_pid,
-    directorate,
+    directorate
 FROM cases_with_accused
