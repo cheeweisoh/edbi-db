6658c38b27d414bdc482279c52013f6428716a46
cheewei <sohcheewei@u.nus.edu>
2026-03-20 11:29:35 +0800

    standardised fact_charge table

--- models/silver/fact_charge.sql
+++ models/silver/fact_charge.sql
@@ -17,6 +17,9 @@ WITH charge_details AS (
         _bronze_loaded_at
     FROM {{ ref('qa_cmplx_criliti_sc_charge_dtls') }}
     WHERE is_valid_row = TRUE
+    {% if is_incremental() %}
+        AND _bronze_loaded_at > (SELECT MAX(_bronze_loaded_at) FROM {{ this }})
+    {% endif %}
 ),
 
 charge_victims AS (
@@ -30,29 +33,9 @@ charge_victims AS (
     FROM {{ ref('qa_info_extracted') }}
     WHERE is_valid_row = TRUE
       AND entity_type = 'Victim'
-),
-
-case_keys AS (
-    SELECT
-        case_skey,
-        case_pid,
-        case_no
-    FROM {{ ref('dim_case') }}
-),
-
-victim_keys AS (
-    SELECT
-        person_skey AS victim_skey,
-        full_name,
-        gender
-    FROM {{ ref('dim_person') }}
-),
-
-committed_date AS (
-    SELECT
-        date_skey,
-        full_date
-    FROM {{ ref('dim_date') }}
+    {% if is_incremental() %}
+        AND _bronze_loaded_at > (SELECT MAX(_bronze_loaded_at) FROM {{ this }})
+    {% endif %}
 ),
 
 fact_case_charge_source AS (
@@ -60,35 +43,44 @@ fact_case_charge_source AS (
         ch.case_pid,
         ch.charge_no,
         ch.charge_type AS offence_type,
-        COALESCE(ch.offence_group, v.offence_group, 'UNKNOWN') AS offence_group,
+        COALESCE(ch.offence_group, cv.offence_group, 'UNKNOWN') AS offence_group,
         ch.charge_status,
-        ch.offence_date,
+        d_commit.date_skey AS committed_date_skey,
         ch._file_date,
-        v.relationship_to_victim AS relation_to_accused,
-        vc.case_skey,
-        vp.victim_skey
+        cv.relationship_to_victim AS relation_to_accused,
+        cases.case_skey,
+        victims.victim_skey,
+        ch._file_date,
+        ch._bronze_loaded_at
     FROM charge_details ch
-    LEFT JOIN case_keys vc
-        ON ch.case_pid = vc.case_pid
-    LEFT JOIN charge_victims v
-        ON vc.case_no = v.case_no
-       AND ch.charge_no = v.charge_no
-    LEFT JOIN victim_keys vp
-        ON v.victim_name = vp.full_name
-       AND v.victim_gender = vp.gender
+    LEFT JOIN {{ ref('dim_case') }} cases
+        ON ch.case_pid = cases.case_pid
+    LEFT JOIN charge_victims cv
+        ON cases.case_no = cv.case_no
+        AND ch.charge_no = cv.charge_no
+    LEFT JOIN {{ ref('dim_person') }} victims
+        ON cv.victim_name = victims.full_name
+        AND cv.victim_gender = victims.gender
+    LEFT JOIN {{ ref('dim_date') }} d_commit
+        ON ch.offence_date = d_commit.full_date
 ),
 
-fact_case_charge_prepped AS (
-    SELECT
-        *,
-        d.date_skey AS committed_date_skey
-    FROM fact_case_charge_source f
-    LEFT JOIN committed_date d
-        ON f.offence_date = d.full_date
+fact_case_charge_deduplicated AS (
+    SELECT *
+    FROM (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY case_pid, charge_no
+                ORDER BY _file_date DESC
+            ) AS rn
+        FROM fact_event_charge_source
+    )
+    WHERE rn = 1
 )
 
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['case_pid', 'charge_no']) }} AS charge_skey,
+    {{ dbt_utils.generate_surrogate_key(['case_pid', 'charge_no']) }} AS case_charge_skey,
     case_skey,
     victim_skey,
     relation_to_accused,
@@ -97,9 +89,6 @@ SELECT
     offence_type,
     offence_group,
     _file_date,
-    _bronze_loaded_at
-FROM fact_case_charge_prepped
-
-{% if is_incremental() %}
-    WHERE _file_date > (SELECT MAX(_file_date) FROM {{ this }})
-{% endif %}
+    _bronze_loaded_at,
+    current_timestamp() AS _silver_loaded_at
+FROM fact_case_charge_deduplicated
