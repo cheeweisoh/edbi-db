e7c10e232ae92b97ee4717d23149971678b2f24d
cheewei <sohcheewei@u.nus.edu>
2026-03-21 16:59:16 +0800

    updated definition of case type

--- models/silver/fact_case_officer.sql
+++ models/silver/fact_case_officer.sql
@@ -40,16 +40,19 @@ case_status AS (
 
 case_flags AS (
     SELECT
-        case_pid,
+        ce.case_pid,
         CASE
-            WHEN MAX(CASE WHEN court_event_type IN ('TRIAL', 'PH') THEN 1 ELSE 0 END) = 1 THEN 'Trial'
-            ELSE 'PG'
+            WHEN MAX(CASE WHEN mapping.grouped_court_event_type = 'Trial' THEN 1 ELSE 0 END) = 1 THEN 'Trial'
+            WHEN MAX(CASE WHEN mapping.grouped_court_event_type = 'PG Mention' THEN 1 ELSE 0 END) = 1 THEN 'PG'
+            ELSE 'TBD'
         END AS case_type,
-        MAX(CASE WHEN court_event_type IN ('TRIAL', 'PH') THEN 1 ELSE 0 END) AS trial_ph_flag
-    FROM {{ ref('qa_tb_criliti_sc_court_events') }}
-    WHERE is_valid_row = TRUE
-        AND court_event_status = 'NEW'
-    GROUP BY case_pid
+        MAX(CASE WHEN mapping.grouped_court_event_type = 'Trial' THEN 1 ELSE 0 END) AS trial_ph_flag
+    FROM {{ ref('qa_tb_criliti_sc_court_events') }} ce
+    LEFT JOIN {{ ref('court_event_type_mapping') }} mapping
+    ON ce.court_event_type = mapping.court_event_type
+    WHERE ce.is_valid_row = TRUE
+        AND ce.court_event_status = 'NEW'
+    GROUP BY ce.case_pid
 ),
 
 first_mention AS (
