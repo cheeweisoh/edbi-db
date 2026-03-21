b8de38a56fe2dde8f462063bcf727afe7c0fe37c
cheewei <sohcheewei@u.nus.edu>
2026-03-21 22:43:02 +0800

    corrected snapshot logic

--- snapshots/snap_assigned_lo.sql
+++ snapshots/snap_assigned_lo.sql
@@ -5,17 +5,28 @@
         target_schema='snapshots',
         unique_key=['case_pid', 'officer_id'],
         strategy='check',
-        check_cols=['_file_date'],
+        check_cols=['officer_name', 'cluster', 'team', '_file_date'],
         invalidate_hard_deletes=True
     )
 }}
 
-SELECT
+WITH ranked AS (
+   SELECT
+        *,
+        row_number() OVER (PARTITION BY case_pid ORDER BY _file_date DESC) rn
+   FROM {{ ref('qa_cmplx_criliti_sc_assigned_lo') }}
+   WHERE is_valid_row = true
+ )
+
+ SELECT
     case_pid,
     officer_id,
+    officer_name,
+    cluster,
+    team,
     _file_date,
     _bronze_loaded_at
-FROM {{ ref('qa_cmplx_criliti_sc_assigned_lo') }}
-WHERE is_valid_row = TRUE
+ FROM ranked
+ WHERE rn = 1
 
 {% endsnapshot %}
